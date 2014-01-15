af1243e3d8ff35af14190b91fbe3b8f0707a0d54
Will Piers <wpiers0405@gmail.com>
2014-01-15 00:08:35 -0700

    moved some board related specs out of the game spec file and into their own file. wrote a few extra specs as well.

---
+++ spec/board_spec.rb
@@ -0,0 +1,42 @@
+require 'spec_helper'
+
+describe Board do
+  describe '#available_spaces' do
+    context "when no moves have been made" do
+      it "returns a flat array with all board spaces" do
+        board = Board.new [[1,2,3],[4,5,6],[7,8,9]]
+        expect(board.available_spaces).to eq [1,2,3,4,5,6,7,8,9]
+      end
+    end
+
+    context "when some moves have been made" do
+      it "returns an array of space numbers" do
+        board = Board.new [[1,2,'O'],['X',5,'O'],['X',8,9]]
+        expect(board.available_spaces).to eq [1,2,5,8,9]
+      end
+    end
+
+    context "when the board is filled" do
+      it "renturns an empty array" do
+        board = Board.new [['O','X','O'],['X','X','O'],['X','O','X']]
+        expect(board.available_spaces).to eq []
+      end
+    end
+  end
+
+  describe '#all_squares_marked?' do
+    context 'when all squares are marked' do
+      it 'is truthy' do
+        board = Board.new [['O','X','X'],['X','X','O'],['X','O','X']]
+        expect(board.all_squares_marked?).to be_true
+      end
+    end
+
+    context 'when some squares are not marked' do
+      it 'is falsy' do
+        board = Board.new [[1,2,3],['O','X','O'],['X','O','X']]
+        expect(board.all_squares_marked?).to be_false
+      end
+    end
+  end
+end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -28,27 +28,4 @@ describe Game do
       expect(@game.is_over?).to be_true
     end
   end
-
-  describe '#available_spaces' do
-    context "when no moves have been made" do
-      it "returns a flat array with all board spaces" do
-        @game.board = Board.new [[1,2,3],[4,5,6],[7,8,9]]
-        expect(@game.board.available_spaces).to eq [1,2,3,4,5,6,7,8,9]
-      end
-    end
-
-    context "when some moves have been made" do
-      it "returns an array of space numbers" do
-        @game.board = Board.new [[1,2,'O'],['X',5,'O'],['X',8,9]]
-        expect(@game.board.available_spaces).to eq [1,2,5,8,9]
-      end
-    end
-
-    context "when the board is filled" do
-      it "renturns an empty array" do
-        @game.board = Board.new [['O','X','O'],['X','X','O'],['X','O','X']]
-        expect(@game.board.available_spaces).to eq []
-      end
-    end
-  end
 end
